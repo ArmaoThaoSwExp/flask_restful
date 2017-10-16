9479e22bb3dfa9fdb05f4ebd0996b7788453f018
Armao Thao <software.exploration@gmail.com>
2017-10-16 01:12:10 -0700

    - Added simple way to return list of tasks using json.

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,15 +1,34 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 
+# Create a Flask app using the name of the file
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, World!\n\n'
 
-@app.route('/testing')
-def testing():
-    return 'TESTING!!!\n\n'
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    """
+    If the user requested to get tasks, return the list of tasks.
+    :return:
+    """
+    return jsonify({'tasks': tasks})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
