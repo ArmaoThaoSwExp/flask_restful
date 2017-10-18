2b2ebf2563b74fa3a9bc45335bedc391b2e7b45c
Armao Thao <software.exploration@gmail.com>
2017-10-17 21:04:48 -0700

    - Added full web service API

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response, request
 
 # Create a Flask app using the name of the file
 app = Flask(__name__)
@@ -21,13 +21,74 @@ tasks = [
 ]
 
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
+# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+# def get_tasks():
+#     """
+#     If the user requested to get tasks, return the list of tasks.
+#     :return:
+#     """
+#     return jsonify({'tasks': tasks})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_tasks(task_id):
     """
-    If the user requested to get tasks, return the list of tasks.
+    If the user requested to get tasks, return the specific task.
     :return:
     """
-    return jsonify({'tasks': tasks})
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title' not in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ''),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
 
 
 if __name__ == '__main__':
