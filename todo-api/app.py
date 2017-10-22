fcccd83b24648cb368888bea954851be6e7c7f6e
Armao Thao <armaothao@gmail.com>
2017-10-21 22:42:31 -0700

    - Modified to add basic auth

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,8 +1,10 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
 
 # Create a Flask app using the name of the file
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 
 tasks = [
@@ -21,6 +23,28 @@ tasks = [
 ]
 
 
+@auth.get_password
+def get_password(username):
+    if username == 'athao':
+        return 'python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_tasks', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
 # @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 # def get_tasks():
 #     """
@@ -35,19 +59,34 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_tasks(task_id):
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
     """
     If the user requested to get tasks, return the specific task.
     :return:
     """
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+    # task = [task for task in tasks if task['id'] == task_id]
+    # if len(task) == 0:
+    #     abort(404)
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+#
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+# @auth.login_required
+# def get_tasks(task_id):
+#     """
+#     If the user requested to get tasks, return the specific task.
+#     :return:
+#     """
+#     task = [task for task in tasks if task['id'] == task_id]
+#     if len(task) == 0:
+#         abort(404)
+#     return jsonify({'task': task[0]})
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+@auth.login_required
 def create_task():
     if not request.json or 'title' not in request.json:
         abort(400)
@@ -62,6 +101,7 @@ def create_task():
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
 
@@ -83,6 +123,7 @@ def update_task(task_id):
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+@auth.login_required
 def delete_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
